971fd4f45e28dd0fae2ac3e4f479aad73d37fc09
Bitan Biswas <bitanxen@gmail.com>
2018-08-24 03:56:13 +0800

    Added cloud sleuth

--- cloud-gateway-server/src/main/java/com/bitanxen/springcloud/cloudgatewayserver/CloudGatewayServerApplication.java
+++ cloud-gateway-server/src/main/java/com/bitanxen/springcloud/cloudgatewayserver/CloudGatewayServerApplication.java
@@ -1,20 +1,20 @@
-package com.bitanxen.springcloud.currencycalculation;
+package com.bitanxen.springcloud.cloudgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableFeignClients("com.bitanxen.springcloud.currencycalculation")
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class CurrencyCalculationApplication {
+public class CloudGatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyCalculationApplication.class, args);
+		SpringApplication.run(CloudGatewayServerApplication.class, args);
 	}
 	
 	@Bean
--- currency-calculation/src/main/java/com/bitanxen/springcloud/currencycalculation/proxy/CurrencyExchangeProxy.java
+++ currency-calculation/src/main/java/com/bitanxen/springcloud/currencycalculation/proxy/CurrencyExchangeProxy.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.bitanxen.springcloud.currencycalculation.model.CurrencyConversion;
 
-@FeignClient(name="currency-exchange")
+@FeignClient(name="cloud-gateway-server")
 @RibbonClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
-	@GetMapping("/currency-exchange/{from}/{to}")
+	@GetMapping("/currency-exchange/currency-exchange/{from}/{to}")
 	public CurrencyConversion retiveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
